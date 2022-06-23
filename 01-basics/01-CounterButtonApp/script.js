1250a379a47fbc0311e6ac0fdb21a6d440edb10a
niiaz <niiaz.gabidullin@gmail.com>
2022-06-23 15:09:12 +0300

    Lesson 2 - counter

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,22 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+// const App = defineComponent({
+//     data(){
+//         return{
+//             counter: 0
+//         }
+//     }
+// });
+// createApp(App);
+
+const app = createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+});
+
+app.mount('#app');
